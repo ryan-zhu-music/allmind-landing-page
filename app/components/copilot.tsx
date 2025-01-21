b1d454d575a894e92605cf1e2fd09a2bf992054d
Ryan Zhu <rz.ryanzhu@gmail.com>
2025-01-21 00:30:29 -0500

    Copilot section, Button component, images

--- app/components/copilot.tsx
+++ app/components/copilot.tsx
@@ -1,9 +1,42 @@
+import Image from "next/image";
 import React from "react";
+import Button from "./ui/button";
 
 export default function Copilot() {
   return (
-    <section className="w-full flex flex-col items-center justify-start h-[200vh] bg-foreground">
-      <div className="container flex flex-col items-center justify-start"></div>
+    <section className="w-full flex flex-col items-center justify-start bg-foreground">
+      <div className="container grid grid-cols-2 px-10 py-14 gap-32">
+        <div className="flex place-content-center">
+          <Image
+            src="/copilot.webp"
+            alt="Copilot"
+            width={1000}
+            height={1000}
+            className="w-full h-auto object-contain"
+          />
+        </div>
+        <div className="flex flex-col gap-6 items-start justify-start text-background">
+          <p className="inline-flex gap-2 font-semibold">
+            <Image src="/icons/star.svg" alt="Star" width={20} height={20} />
+            NEW FEATURE
+          </p>
+          <h2>Introducing OxyCopilot</h2>
+          <p className="text-2xl">
+            Speed up data collection with{" "}
+            <strong>the first AI-powered assistant</strong> designed to handle
+            diverse website structures and meet your data extraction needs
+            without manual coding.
+          </p>
+          <ul className="list-disc list-inside text-2xl">
+            <li>Start scraping using English language prompts</li>
+            <li>Parse raw HTML into structured data </li>
+            <li>Scale through the Web Scraper API platform</li>
+          </ul>
+          <Button variant="outline-background" size="lg" className="mt-10">
+            Try for free
+          </Button>
+        </div>
+      </div>
     </section>
   );
 }
---
+++ app/components/ui/button.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  className?: string;
+  variant:
+    | "primary"
+    | "secondary"
+    | "outline-foreground"
+    | "outline-background";
+  size?: "sm" | "md" | "lg";
+  onClick?: () => void;
+}
+
+const variants = {
+  primary: "bg-mint text-foreground",
+  secondary: "bg-navy-dark text-background",
+  "outline-foreground": "border border-foreground text-foreground",
+  "outline-background": "border border-background text-background",
+};
+
+const sizes = {
+  sm: "px-2 py-1.5 text-sm",
+  md: "px-3 py-2 text-base",
+  lg: "px-4 py-3 text-lg",
+};
+
+export default function Button({
+  children,
+  className,
+  variant,
+  size = "md",
+  onClick,
+}: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={`rounded-md font-semibold ${variants[variant]} ${sizes[size]} ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
