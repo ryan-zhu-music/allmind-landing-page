d55acfc403c0805a1e074f94df98202a5a20113f
Ryan Zhu <rz.ryanzhu@gmail.com>
2025-01-21 00:29:08 -0500

    Hero, Navbar, theme kit

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,9 +1,57 @@
 import React from "react";
+import Button from "./ui/button";
+import Image from "next/image";
+
+const partners = [
+  "trivago",
+  "seon",
+  "debunk",
+  "conductor",
+  "stanford",
+  "boltive",
+  "wiser",
+];
 
 export default function Hero() {
   return (
     <header className="w-full flex flex-col items-center justify-start h-screen">
-      <div className="container flex flex-col items-center justify-start"></div>
+      <div className="container flex flex-col items-center justify-end h-full">
+        <div className="w-full flex flex-nowrap">
+          <div className="w-1/2 flex flex-col items-start justify-center gap-8">
+            <h1>Easy Access to Web Data at Scale</h1>
+            <p className="text-stretch-lg text-xl">
+              Powered by the world&apos;s largest ethical proxy network, AI and
+              ML-driven Scraper APIs, and tailored no-code datasets.
+            </p>
+            <div className="flex gap-5">
+              <Button variant="primary">Try Oxylabs today</Button>
+              <Button variant="outline-foreground">Start with Google</Button>
+            </div>
+          </div>
+          <div className="w-1/2 flex place-content-center">
+            <Image
+              src="/hero-graphic.webp"
+              alt="Hero graphic"
+              width={1000}
+              height={1000}
+            />
+          </div>
+        </div>
+        <p className="font-semibold text-lg">
+          A trusted partner for 3,500+ clients globally
+        </p>
+        <div className="w-full flex items-center justify-center gap-10 py-10">
+          {partners.map((partner) => (
+            <Image
+              key={partner}
+              src={`/partners/${partner}.webp`}
+              alt={partner}
+              width={130}
+              height={130}
+            />
+          ))}
+        </div>
+      </div>
     </header>
   );
 }
--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,13 +1,35 @@
 import React from "react";
+import Image from "next/image";
+import Button from "./ui/button";
 
 export default function Nav() {
   return (
-    <nav className="w-full flex flex-col items-center justify-start sticky top-0">
+    <nav className="w-full flex flex-col items-center justify-start fixed top-0">
       <div className="bg-secondary h-14 w-full">
-        <div className="container flex flex-col items-center justify-between"></div>
+        <div className="container flex h-full gap-8 items-center justify-between">
+          <p className="font-semibold">Proxy locations</p>
+          <p className="font-semibold">Network status</p>
+          <p className="font-semibold">Careers</p>
+          <div className="flex-grow" />
+          <p>hello@oxylabs.io</p>
+          <p className="font-semibold">English (EN)</p>
+          <p className="font-semibold">Log in</p>
+        </div>
       </div>
-      <div className="w-full h-24 bg-background">
-        <div className="container flex flex-col items-center justify-start"></div>
+      <div className="w-full h-20 bg-background shadow-lg">
+        <div className="container flex h-full items-center justify-start gap-8">
+          <Image src="/logo.svg" alt="Oxylabs logo" width={100} height={100} />
+          <p className="text-lg font-medium">Proxies</p>
+          <p className="text-lg font-medium">Web Scraper API</p>
+          <p className="text-lg font-medium">Datasets</p>
+          <p className="text-lg font-medium">Pricing</p>
+          <p className="text-lg font-medium">Developers</p>
+          <p className="text-lg font-medium">Solutions</p>
+          <div className="flex-grow" />
+          <Button variant="secondary" size="sm">
+            <p>Try Oxylabs today</p>
+          </Button>
+        </div>
       </div>
     </nav>
   );
