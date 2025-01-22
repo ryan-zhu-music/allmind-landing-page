91d4accd94c081a6bab0c047a563eecf637f9ef2
Ryan Zhu <rz.ryanzhu@gmail.com>
2025-01-21 22:59:04 -0500

    Footer section

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,10 +1,27 @@
+import Image from "next/image";
 import React from "react";
+import Button from "./ui/button";
 
 export default function Footer() {
   return (
     <footer className="w-full flex flex-col items-center justify-start bg-foreground h-screen">
-      <div className="w-full h-20 bg-mint-light">
-        <div className="container flex items-center justify-between"></div>
+      <div className="w-full h-24 py-6 bg-mint-light">
+        <div className="container flex h-full items-center justify-between gap-6">
+          <Image
+            src="/graphics/scrapingdigest.svg"
+            alt="ScrapingDigest"
+            width={220}
+            height={100}
+            className="object-contain"
+          />
+          <div className="flex-grow" />
+          <p className="font-semibold">
+            Get the latest news from data gathering world
+          </p>
+          <Button variant="primary" size="lg">
+            I&apos;m interested
+          </Button>
+        </div>
       </div>
       <div className="container flex flex-col items-center justify-start"></div>
     </footer>
